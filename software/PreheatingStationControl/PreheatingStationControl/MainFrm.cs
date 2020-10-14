ef31d3e6896cb98552af33ed6c16c4ba9e285956
smoluks <smoluks4096@rambler.ru>
2020-10-15 00:03:20 +0400

    add winforms client boilerplate

---
+++ software/PreheatingStationControl/PreheatingStationControl/MainFrm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace PreheatingStationControl
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+    }
+}
